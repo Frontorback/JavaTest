090fefa3771c026eb9d8a6d7f830e78c388f5187
Frontorback <artemkravzovv@gmail.com>
2020-10-14 15:22:56 +0300

    test

---
+++ test.js
@@ -0,0 +1,43 @@
+
+function getAge(minAge=1,maxAge=150) {
+const age = prompt("How old are you", "age")
+const isDataCorrect = confirm(`Вам ${age} лет?`);
+console.log(isDataCorrect);
+if (age === null || age <=minAge || age>=maxAge){
+console.log("Отказ");
+return getAge(minAge=1,maxAge=150);
+}
+else if (age.length >3){
+    console.log ("max 3");
+    return getAge();
+}
+else if (!Number(parseInt(age))){
+    console.log ("only numbers");
+    return getAge();
+}
+else {
+console.log(`Вам, ${age}`);
+return age;
+}
+
+}
+getAge();
+
+function getAgeByString(age){
+
+    const ageStr = age.toString();
+    const lastNumber = +ageStr[ageStr.length - 1];
+    const lastTwoDigits = age % 100;
+
+    if (lastNumber === 1 && lastTwoDigits !==11){
+        return age + `год`;
+    
+    }
+    if ((lastNumber >1 && lastNumber  >5) || (lastTwoDigits <=11 && lastTwoDigits >=15)){
+        return `${age} года`;
+
+    }
+    return `${age} лет`
+}
+
+getAgeByString();
